10ec538bda596c8d01821297fba47e6ddbbd27ee
Sylvanus <sylvanuszhy@gmail.com>
2016-05-25 13:15:26 +0800

    fetch info in list

--- routes/chat/conversation.js
+++ routes/chat/conversation.js
@@ -4,9 +4,24 @@
 
 var express = require('express');
 var router = express.Router();
+var Models = require('../../models/models');
 
 router.get('/', function(req, res, next) {
-    res.render('chat/conversation', { title: 'Chats' });
+    if (req.session.uid == null) {
+        res.redirect('/login');
+    }
+    var User = Models.User;
+    User.find({ email: req.session.uid }, 'chats', function (err, docs) {
+        if (docs.length == 1) {
+            res.render('chat/conversation', {
+                title: 'Chats',
+                chats: docs[0].chats
+            });
+        } else {
+            console.log('wrong email');
+            res.redirect(303, '/login');
+        }
+    });
 });
 
 module.exports = router;
